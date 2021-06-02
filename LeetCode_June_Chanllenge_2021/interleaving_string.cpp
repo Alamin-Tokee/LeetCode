19f8ef905858c38e87ee5fc0f0ef19897a3c24d7
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-02 22:58:00 +0600

    Using one 1d arrar to solve

--- LeetCode_June_Chanllenge_2021/interleaving_string.cpp
+++ LeetCode_June_Chanllenge_2021/interleaving_string.cpp
@@ -1,5 +1,6 @@
 //Time Complexity O(n*m)
 //Space Complexity O(n*m)
+//2D Array Using
 
 class Solution {
 public:
@@ -28,3 +29,25 @@ public:
     }
 };
 
+//Time Complexity O(n*m)
+//Space Complexity O(n*m)
+//2D Array Using
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        #define S1 (dp[j] && s1[i-1] == s3[i+j-1])
+        #define S2 (dp[j-1] && s2[j-1] == s3[i+j-1])
+        
+        int n = s1.size(), m = s2.size(), k = s3.size();
+        if (n + m != k) return false;
+        vector<bool> dp(m+1, 0);
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[j] = (i==0 && j==0) ? true : i==0 ? S2 : j==0 ? S1 : S1 || S2;
+            }
+        }
+
+        return dp[m];
+    }
+};
