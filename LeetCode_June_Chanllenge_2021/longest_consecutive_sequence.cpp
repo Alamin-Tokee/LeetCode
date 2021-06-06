aaa2a19feb30dd4108ccc5152d4e9e61f03d3001
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-06 16:38:47 +0600

    June leetcode challenge third approach

--- LeetCode_June_Chanllenge_2021/longest_consecutive_sequence.cpp
+++ LeetCode_June_Chanllenge_2021/longest_consecutive_sequence.cpp
@@ -68,3 +68,32 @@ public:
     }
 };
 
+//Third approach using set
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(),nums.end());
+        
+        int longest=0;
+        
+        for(auto& num: s){
+            if(s.count(num-1)) continue;
+            
+            int j=1;
+            
+            while(s.count(num+j)) j++;
+            
+            longest=max(longest,j);
+        }
+        
+        return longest;
+    }
+};
+
+//Time Complexity o(n)
+
+//Space Complexity O(1)
+
+
+
