614a2cf412b2bc96446c4f6d522968da1eab4d42
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-09 00:19:43 +0600

    Binary tree create from inorder and postorder

--- 106_construct_binary_tree_from_inorder_and_postorder.cpp
+++ 106_construct_binary_tree_from_inorder_and_postorder.cpp
@@ -25,3 +25,29 @@ public:
         return constructTree(postorder,0 , postorder.size()-1);
     }
 };
+
+//Time Complexity O(n)
+//Space Complexity O(1)
+
+class Solution {
+public:
+     TreeNode* constructTree(vector<int>& postorder,int pl,int pr,vector<int>& inorder,int il,int ir){
+         if(pl > pr || il > ir) return NULL;
+         
+         int i=0;
+         for(i=il;i<=ir;i++){
+             if(postorder[pr]==inorder[i]) break;
+         }
+         
+         TreeNode* root=new TreeNode(postorder[pr]);
+         
+         root->left=constructTree(postorder,pl,pl+i-il-1,inorder,il,i-1);
+         root->right=constructTree(postorder,pl+i-il,pr-1,inorder,i+1,ir);
+         
+         return root;
+     }
+    
+     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+         return constructTree(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1);
+     }
+};
