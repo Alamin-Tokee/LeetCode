35b591af7cdcf9e7cd793dcf69639b1bc3d98a0d
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-09 23:29:02 +0600

    June leetcode challenge jump game vi

--- LeetCode_June_Chanllenge_2021/jume_game_vi.cpp
+++ LeetCode_June_Chanllenge_2021/jume_game_vi.cpp
@@ -34,4 +34,24 @@ public:
      }
  };
 
- 
+//Using Deque 
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        deque<int>dq{0};
+        for(int i = 1; i < n; ++i){
+            nums[i]=nums[dq.front()]+nums[i];
+            
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
+            dq.push_back(i);
+            
+            if(i-dq.front() >= k) dq.pop_front();
+        }
+        
+        return nums[n-1];
+    }
+};
+
+
