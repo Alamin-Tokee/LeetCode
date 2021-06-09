20ea6eea1569cf5469778a6d1bd1d307f5b85837
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-09 23:27:48 +0600

    June leetcode challenge jump game vi

--- LeetCode_June_Chanllenge_2021/jume_game_vi.cpp
+++ LeetCode_June_Chanllenge_2021/jume_game_vi.cpp
@@ -1,3 +1,4 @@
+//Using Priority Queue
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
@@ -15,3 +16,22 @@ public:
         return nums[n-1];
     }
 };
+
+//Using Multiset
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+         int n=nums.size();
+         multiset<int,greater<int>>ms;
+         ms.insert(nums[0]);
+         for(int i=1;i<n;i++){
+             nums[i]+= ms.empty() ? 0 : *ms.begin();
+             ms.insert(nums[i]);
+            
+             if(ms.size()>k) ms.erase(ms.find(nums[i-k]));
+         }
+         return nums.back();
+     }
+ };
+
+ 
