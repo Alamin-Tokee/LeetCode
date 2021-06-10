cb4fb9e9df3eccdb8d8e31e1be0211ffa03ad781
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-10 23:31:48 +0600

    June leetcode challenge my calender one

--- LeetCode_June_Chanllenge_2021/my_calender_one.cpp
+++ LeetCode_June_Chanllenge_2021/my_calender_one.cpp
@@ -1,5 +1,5 @@
 //First Approach
-//Time Complexity O(n)
+//Time Complexity O(n^2)
 //Space Complexity O(n)
 
 class MyCalendar {
@@ -14,5 +14,23 @@ public:
         vec.push_back({start,end});
         
         return true;
+    }
+};
+
+// Binary Search And Set
+// Time Complexity O(NlogN)
+// Space Complexity O(N)
+
+class MyCalendar {
+    set<pair<int, int>> store; 
+public:
+    MyCalendar() {}
+    
+    bool book(int start, int end) {
+        auto it = store.lower_bound({start, end}); 
+        if (it != store.begin() && prev(it)->second > start) return false; 
+        if (it != store.end() && end > it->first) return false; 
+        store.insert({start, end}); 
+        return true; 
     }
 };
