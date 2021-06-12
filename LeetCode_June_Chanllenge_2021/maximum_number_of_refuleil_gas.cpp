4097bfb31eef9a5800174ee6f11a0a6ba7f3077a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-12 17:32:57 +0600

    June leetcode challenge maximum refuel better solution

--- LeetCode_June_Chanllenge_2021/maximum_number_of_refuleil_gas.cpp
+++ LeetCode_June_Chanllenge_2021/maximum_number_of_refuleil_gas.cpp
@@ -25,3 +25,28 @@ public:
         return -1;
 	}
 };
+
+// Priority Queue solution(Max Heap)
+// Time Complexity O(NlogN)
+// Space Complexity o(N)
+
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        priority_queue<int>pq;
+        int distance=startFuel;
+        int i=0,ans=0;
+        
+        while(distance < target){
+            while(i < stations.size() && stations[i][0] <= distance){
+                pq.push(stations[i++][1]);
+            }
+            if(pq.empty()) return -1;
+            distance+=pq.top();
+            pq.pop();
+            ans++;
+        }
+        
+        return ans;
+    }
+};
