bcc68a2f3d20815f6b61f7b74f633f2ab36c7424
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-16 20:25:35 +0600

    Add some imporatant note

--- LeetCode_June_Chanllenge_2021/matchsticks_to_square.cpp
+++ LeetCode_June_Chanllenge_2021/matchsticks_to_square.cpp
@@ -28,3 +28,57 @@ public:
         return solve(0,sum,x/4,matchsticks);
     }
 };
+
+// Important Note
+class Solution {
+public:
+    bool makesquare(vector<int>& nums) {
+        
+        if (nums.size() == 0)
+            return false;
+        
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        
+        if (sum % 4 != 0) return false;
+        
+        int target = sum / 4;
+        
+        sort(nums.begin(), nums.end(), greater<int>());
+        
+        if (any_of(nums.begin(), nums.end(), [&](int v) { return v > target; }))
+            return false;
+        
+        int n = nums.size();
+        
+        vector<bool> visited(n, false);
+        
+        return find(nums, 4, target, 0, 0, visited);
+    }
+    
+    bool find(vector<int>& nums, int k, int target, int sum, int start, vector<bool>& visited)
+    {
+        if (k == 1) return true;
+        
+        if (sum > target)
+            return false;
+        else if (sum == target)
+        {
+            return find(nums, k-1, target, 0, 0, visited);
+        }
+        
+        for (int i = start; i < nums.size(); i++)
+        {
+            if (sum + nums[i] > target) continue;
+            
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            
+            if (find(nums, k, target, sum + nums[i], i+1, visited) ) return true;
+            
+            visited[i] = false;
+        }
+        
+        return false;
+    }
+};
