ac0bfea271bcedd289e2752f89f335b319347c8a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-17 21:17:46 +0600

    Another approch for number of subarray with bound

--- LeetCode_June_Chanllenge_2021/number_of_subarray_with_bound.cpp
+++ LeetCode_June_Chanllenge_2021/number_of_subarray_with_bound.cpp
@@ -25,3 +25,21 @@ public:
         return count;
     }
 };
+
+//Two Pointer Approach
+//Time Complexity O(n)
+//Space Complexity O(1)
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int ans=0, low=0, mid=0;
+        for(auto num : nums){
+            if(num > right) mid = 0;
+            else ans+=++mid;
+            if(num>=left) low=0;
+            else ans-=++low;
+        }
+        
+        return ans;
+    }
+};
