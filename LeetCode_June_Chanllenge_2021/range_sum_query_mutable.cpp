d6d83cc0048ea951165490fa640b2605a42203e2
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-18 20:12:48 +0600

    June leetcode challenge range sum query binary index tree

--- LeetCode_June_Chanllenge_2021/range_sum_query_mutable.cpp
+++ LeetCode_June_Chanllenge_2021/range_sum_query_mutable.cpp
@@ -137,3 +137,46 @@ public:
  * obj->update(index,val);
  * int param_2 = obj->sumRange(left,right);
  */
+
+
+//Binary Index Tree
+// Time Complexity: Sum: O(logn), Update: O(logn)
+// Space Complexity: O(n) for the segment tree
+// NOTE:
+
+class NumArray {
+public:
+    vector<int> bt, vv;
+    int n;
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        vv = nums;
+        bt.resize(n, 0);
+        
+        for(int i = 0; i < n; ++i) {
+            add(i, nums[i]);
+        }
+    }
+    
+    void add(int ind, int val) {
+        for(; ind < n; ind = ind | (ind + 1))
+            bt[ind] += val;
+    }
+    
+    void update(int ind, int val) {
+        int diff = val - vv[ind];
+        vv[ind] = val;
+        add(ind, diff);
+    }
+    
+    int sum(int r) {
+        int ret = 0;
+        for(; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bt[r];
+        return ret;
+    }
+    
+    int sumRange(int left, int right) {
+        return sum(right) - sum(left - 1);
+    }
+};
