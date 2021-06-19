5aab3f9745fedcadc2a4cda39e656b618786e23b
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-19 23:45:49 +0600

    Add bruth force in k pair of array

--- LeetCode_June_Chanllenge_2021/k_inverse_pair_array.cpp
+++ LeetCode_June_Chanllenge_2021/k_inverse_pair_array.cpp
@@ -1,3 +1,20 @@
+
+// Time: O(n * k * k)
+// Memory: O(n * k)
+
+int dp[1001][1001] = {};
+int kInversePairs(int n, int k) {
+    if (k <= 0)
+        return k == 0;
+    if (dp[n][k] == 0) {
+        dp[n][k] = 1;
+        for (auto i = 0; i < n; ++i) {
+            dp[n][k] = (dp[n][k] + kInversePairs(n - 1, k - i)) % 1000000007;
+        }
+    }
+    return dp[n][k] - 1;
+}
+
 //Time:O(n*k)
 //Space:O(n*k)
 class Solution {
