abdf95a0f2bea4f53534c373db114be1e10e9915
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-20 18:46:01 +0600

    Swim in rising update

--- LeetCode_June_Chanllenge_2021/swim_in_rising_water.cpp
+++ LeetCode_June_Chanllenge_2021/swim_in_rising_water.cpp
@@ -53,3 +53,64 @@ public:
         return ans;
     }
 };
+
+// Method 2 - Djikstra's Algorithm
+// T=O(V^2)=O(n^4)
+
+class Solution {
+public:
+    vector<pair<int,int>>directions{{0,1},{-1,0},{0,-1},{1,0}};
+    
+    bool isValid(vector<vector<bool>>& visited,int r,int c){
+        if(r < 0 || c < 0 || r >= visited.size() || c >= visited[0].size() || visited[r][c]) return false;
+        
+        return true;
+    }
+    
+    pair<int,int> findMin(vector<vector<bool>>& visited, vector<vector<int>>& distance){
+        int minDis=INT_MAX;
+        pair<int,int>minIndex={-1,-1};
+        for(int i=0;i<distance.size();i++){
+            for(int j=0;j<distance.size();j++){
+                if(!visited[i][j] && distance[i][j] < minDis){
+                    minDis=distance[i][j];
+                    minIndex={i,j};
+                }
+            }
+        }
+        
+        return minIndex;
+    }
+    
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        
+        vector<vector<bool>>visited(n,vector<bool>(n,false));
+        vector<vector<int>>distance(n,vector<int>(n,INT_MAX));
+        distance[0][0]=grid[0][0];
+        int done=0;
+        
+        while(done!=n*n){
+            pair<int,int> u=findMin(visited,distance);
+            
+            int i=u.first;
+            int j=u.second;
+            
+            visited[i][j]=true;
+            done++;
+            
+            for(int k=0;k<4;k++){
+                int ii=i+directions[k].first;
+                int jj=j+directions[k].second;
+                
+                if(isValid(visited,ii,jj)){
+                    int possibleDistance=max(distance[i][j],grid[ii][jj]);
+                    if(possibleDistance < distance[ii][jj]) {
+                        distance[ii][jj]=possibleDistance;
+                    }
+                }
+            }
+        }
+        return distance[n-1][n-1];
+    }
+};
