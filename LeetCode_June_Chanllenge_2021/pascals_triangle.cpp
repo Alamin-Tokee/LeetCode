df8cc4be4a9a25325f24b58bf6258aa31653479a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-21 23:53:24 +0600

    June challenge math formula add

--- LeetCode_June_Chanllenge_2021/pascals_triangle.cpp
+++ LeetCode_June_Chanllenge_2021/pascals_triangle.cpp
@@ -49,3 +49,33 @@ public:
         return vec;
     }
 };
+
+
+//Math Formula
+class Solution {
+public:
+    vector<int> getRow(int line) {
+        int prev = 1;
+        vector<int> temp{1};
+        for(int i = 1; i<line+1; i++) {
+            int curr = ((line-i+1) * prev)/i;
+            temp.push_back(curr);
+            prev = curr;
+        }
+        return temp;
+    }
+    
+    vector<vector<int>> generate(int line) {
+        vector<vector<int>> result;
+        result.push_back({1});
+        if(line == 1) {
+            return result;
+        }
+        
+        for(int lineNo = 1; lineNo < line; lineNo++) {
+            result.push_back(getRow(lineNo));
+        }
+        
+        return result;
+    }
+};
