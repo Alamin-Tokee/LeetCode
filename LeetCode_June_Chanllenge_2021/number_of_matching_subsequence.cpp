5dd3798311f5bcf235af265ff9347fc219c8315a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-22 20:52:22 +0600

    June leetcode challenge number of matching sequence

--- LeetCode_June_Chanllenge_2021/number_of_matching_subsequence.cpp
+++ LeetCode_June_Chanllenge_2021/number_of_matching_subsequence.cpp
@@ -1,3 +1,31 @@
+//Backtracking(Time Limit Execced)
+
+class Solution {
+public:
+	vector<sting> sub;
+	void findAllSub(string input,int n,int index=-1,string curr=""){
+		if(index==n) return;
+
+		sub.push_back(curr);
+
+		for(int i=index+1;i<n;i++){
+			curr=curr+index[i];
+			findAllSub(input,n,i,curr);
+			curr=curr.erase(curr.size()-1);
+		}
+	}
+	int numMatchingSubseq(string s, vector<string>& words) {
+		int count=0;
+		findAllSub(s,s.size());
+		for(string w:words){
+			if(find(sub.begin(),sub.end(),w)!=sub.end()) count++;
+		}
+		return count;
+	}
+};
+
+
+//Optimize solution using hasing
 class Solution {
 public:
     bool matchString(string a,string b){
