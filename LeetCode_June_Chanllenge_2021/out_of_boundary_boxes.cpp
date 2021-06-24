d2289041c8dad443f4b3a7c293cadaf17cc40955
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-24 20:32:15 +0600

    out of boundary boxes updated

--- LeetCode_June_Chanllenge_2021/out_of_boundary_boxes.cpp
+++ LeetCode_June_Chanllenge_2021/out_of_boundary_boxes.cpp
@@ -15,3 +15,33 @@ public:
             +findPaths(m,n,maxMove-1,startRow,startColumn+1);
     }
 };
+
+//Using Memoization(Top Down)
+//Time Complexity:O(mnN)
+//Space Complexity O(mnN)
+
+class Solution {
+public:
+    int dp[51][51][52];
+    int mod=1e9+7;
+    int countPaths(int m, int n, int x, int i, int j){
+        if(i==m || j==n || i < 0 || j < 0) return 1;
+        
+        if(x==0) return 0;
+        if(dp[i][j][x]!=-1) return dp[i][j][x];
+        
+        int ans=0;
+        ans=(ans+countPaths(m,n,x-1,i-1,j))%mod;
+        ans=(ans+countPaths(m,n,x-1,i+1,j))%mod;
+        ans=(ans+countPaths(m,n,x-1,i,j-1))%mod;
+        ans=(ans+countPaths(m,n,x-1,i,j+1))%mod;
+        
+        return dp[i][j][x]=ans;
+        
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp,-1,sizeof(dp));
+        
+        return countPaths(m,n,maxMove,startRow,startColumn);
+    }
+};
