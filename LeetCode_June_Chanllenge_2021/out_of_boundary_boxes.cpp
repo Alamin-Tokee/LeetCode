e57090514778e5351512c5ab027335a9db398194
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-24 20:34:13 +0600

    Tabulation out of boundary paths

--- LeetCode_June_Chanllenge_2021/out_of_boundary_boxes.cpp
+++ LeetCode_June_Chanllenge_2021/out_of_boundary_boxes.cpp
@@ -1,4 +1,4 @@
-//Using Recursion
+//Using Recursion(Brute Force)
 //Time Complexity:O(4^n)
 //Space Complexity O(n)
 
@@ -45,3 +45,104 @@ public:
         return countPaths(m,n,maxMove,startRow,startColumn);
     }
 };
+
+//Using Tabulation(Bottm Up)
+//Time Complexity:O(mnN)
+//Space Complexity O(mnN)
+
+class Solution {
+public:
+    vector<vector<int>> dir{{-1,0},{0,1},{1,0},{0,-1}};
+    int mod = 1e9 + 7;
+    
+    int fn_memo(int m,int n,int sr,int sc,int msf,int maxMoves,vector<vector<vector<int>>>& dp){
+        
+        if(msf > maxMoves) return 0;
+        
+        if(msf <= maxMoves)
+            if(sr < 0 || sc < 0 || sr == m || sc == n) return 1;
+        
+        if(dp[sr][sc][msf] != -1) return dp[sr][sc][msf];
+        
+        long count = 0;
+        for(int d=0; d < 4; d++){
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            
+            if(msf <= maxMoves)
+                count = (count + fn_memo(m,n,r,c,msf + 1,maxMoves,dp) % mod) % mod;
+        }
+        
+        return dp[sr][sc][msf] = count % mod;
+    }
+    
+    int findPaths(int m, int n, int maxMoves, int x, int y) {
+        if(m == 1 && n == 1) return 4;
+        if(maxMoves == 0) return 0;
+        
+        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(maxMoves + 1,-1)));
+        int ans = fn_memo(m,n,x,y,0,maxMoves,dp);
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    int findPaths(int m, int n, int N, int i, int j) {
+        auto dp=vector<vector<int>>(m,vector<int>(n,0));
+        auto temp=dp;
+        
+        int M=1e9+7;
+        
+        for(int k=0;k<N;k++){
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    long a=(i-1<0) ? 1: dp[i-1][j];
+                    long b=(i+1>=m) ? 1: dp[i+1][j];
+                    long c=(j-1<0) ? 1: dp[i][j-1];
+                    long d=(j+1>=n) ? 1: dp[i][j+1];
+                    
+                    temp[i][j]=(a+b+c+d)%M;
+                }
+            }
+            dp=temp;
+        }
+        return dp[i][j];
+    }
+};
+
+class Solution {
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        
+        vector<int> dirs{0, -1, 0, 1, 0};
+        
+        int mod = 1e9 + 7;
+        
+        for (int k = 0; k < maxMove; ++k) {   
+            vector<vector<int>> tmp(m, vector<int>(n));
+            
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    for (int x = 0; x < 4; ++x) {
+                        auto dx = i + dirs[x];
+                        auto dy = j + dirs[x + 1];
+                        
+                        if (dx < 0 || dy < 0 || dx >= m || dy >= n) {
+                            tmp[i][j] += 1;
+                        } else {
+                            tmp[i][j] = (tmp[i][j] + dp[dx][dy]) % mod;
+                        }
+                    }
+                }
+            }
+            
+            dp = tmp;
+        }
+        
+        return dp[startRow][startColumn];
+    }
+};
+
