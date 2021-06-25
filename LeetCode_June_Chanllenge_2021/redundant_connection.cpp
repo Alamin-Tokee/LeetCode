ec2d54b33851ba0172c03c12486271dec60dab2a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-25 16:27:13 +0600

    Redundant connecton union find solution

--- LeetCode_June_Chanllenge_2021/redundant_connection.cpp
+++ LeetCode_June_Chanllenge_2021/redundant_connection.cpp
@@ -73,4 +73,35 @@ public:
     }
 };
 
+//Union find solution 
+
+class Solution {
+public:
+    vector<int>parent;
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> ans(2);
+        parent.resize(edges.size()+1,-1);
+        ans=detectCycle(edges);
+        return ans;
+    }
+    void unionOperation(int from,int to){
+        from=find(from);
+        to=find(to);
+        parent[from]=to;
+    }
+    int find(int v){
+        if(parent[v]==-1) return v;
+        else return find(parent[v]);
+    }
+    vector <int> detectCycle(vector<vector<int>>& edges){
+        vector<int> ans(2);
+        for(int i=0;i<edges.size();i++){
+            if(find(edges[i][0])==find(edges[i][1])) {
+            ans[0]=edges[i][0]; ans[1]=edges[i][1]; break;
+            }
+            else unionOperation(find(edges[i][0]),find(edges[i][1]));
+        }
+       return ans;
+    }
+};
 
