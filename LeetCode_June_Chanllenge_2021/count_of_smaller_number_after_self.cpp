6d847f45a1fe46807617b71cb0e1327d4c250104
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-26 22:54:12 +0600

    updated

--- LeetCode_June_Chanllenge_2021/count_of_smaller_number_after_self.cpp
+++ LeetCode_June_Chanllenge_2021/count_of_smaller_number_after_self.cpp
@@ -47,20 +47,6 @@ public:
         
     }
     vector<int> countSmaller(vector<int>& nums) {
-//         int n=nums.size();
-//         vector<int>res;
-//         int c=0;
-//         for(int i=0;i<n;i++){
-//             for(int j=i;j<n;j++){
-//                 if(nums[i] > nums[j]){
-//                    c++;
-//                 }
-//             }
-//             res.push_back(c);
-//             c=0;
-//         }
-        
-//         return res;
         memset(seg,0,sizeof(seg));
         int n=nums.size();
         vector<int> ans(n);
@@ -69,5 +55,62 @@ public:
             update(0,20001,1,nums[i]+10001);
         }
         return ans;
+    }
+};
+
+
+//Merge Sort
+
+class Solution {
+public:
+    int ans[100004];
+    void merge(int l,int mid,int r,vector<pair<int,int> > &v){
+        vector<pair<int,int>>temp(r-l+1);
+        int ct=0;
+        int i=l,j=mid+1,k=0;
+        while(i<=mid && j<=r){
+            if(v[i].first<=v[j].first){
+                ans[v[i].second]+=ct;
+                temp[k]=v[i];i++;k++;
+            }
+            else{
+                temp[k]=v[j];
+                ct++;j++;k++;
+            }
+        }
+        while(i<=mid)
+        {
+            ans[v[i].second]+=ct;
+            temp[k]=v[i];i++,k++;
+        }
+        while(j<=r){
+            temp[k]=v[j];j++,k++;
+        }
+        
+        k=0;
+        for(i=l;i<=r;i++){
+            v[i]=temp[k];k++;
+        }
+    }
+    void mergeSort(vector<pair<int,int> > &v,int l,int r){
+        if(l<r){
+            int mid = (l+r)/2;
+            mergeSort(v,l,mid);
+            mergeSort(v,mid+1,r);
+            merge(l,mid,r,v);
+        }
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+        memset(ans,0,sizeof(ans));
+        vector<pair<int,int> > v;
+        
+        for(int i=0;i<nums.size();i++){
+            v.push_back({nums[i],i});
+        }
+        mergeSort(v,0,nums.size()-1);
+        
+        vector<int> res;
+        for(int i=0;i<nums.size();i++) res.push_back(ans[i]);
+        return res;
     }
 };
