a7708bfc773f14199d4b78ef3d27105a15147c45
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-30 23:48:59 +0600

    Lowest common ancestor updated

--- LeetCode_June_Chanllenge_2021/lowest_common_ancestor_of_binary_tree.cpp
+++ LeetCode_June_Chanllenge_2021/lowest_common_ancestor_of_binary_tree.cpp
@@ -17,5 +17,16 @@ public:
     }
 };
 
+//Time Complexity O(n)
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root; 
+        TreeNode *left = lowestCommonAncestor(root->left, p, q), *right = lowestCommonAncestor(root->right, p, q); 
+        if (left && right) return root; 
+        return left ? left : right; 
+    }
+};
 
 
