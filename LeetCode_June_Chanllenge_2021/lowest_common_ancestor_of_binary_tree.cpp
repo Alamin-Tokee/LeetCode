5f27edf580cc1d1595a92a85940c76622ba36e5a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-06-30 23:47:43 +0600

    Lowest common Ancestor of a Binary Tree

---
+++ LeetCode_June_Chanllenge_2021/lowest_common_ancestor_of_binary_tree.cpp
@@ -0,0 +1,21 @@
+//Time Complexity O(n)
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        if(!root) return NULL;
+        if(root->val==p->val || root->val==q->val) return root;
+        
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+        if(left && right) return root;
+        
+        return left ? left : right;   
+    }
+};
+
+
+
