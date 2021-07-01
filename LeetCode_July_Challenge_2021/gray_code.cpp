eccfe7455b1f325ddc64bea22d3e41e9d60a9c4b
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-01 19:51:59 +0600

    Updated gray code

--- LeetCode_July_Challenge_2021/gray_code.cpp
+++ LeetCode_July_Challenge_2021/gray_code.cpp
@@ -40,3 +40,24 @@ public:
         return result;
     }
 };
+
+
+//Iterative Approach
+// Time complexity: O(2 ^ n)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        result.push_back(0);
+        
+        for (int i = 1; i <= n; i++) {
+            int previousSequenceLength = result.size();
+            int mask = 1 << (i - 1);
+            for (int j = previousSequenceLength - 1; j >= 0; j--) {
+                result.push_back(mask + result[j]);
+            }
+        }
+        return result;
+    }
+};
