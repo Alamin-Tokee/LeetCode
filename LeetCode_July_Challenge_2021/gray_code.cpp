d94fbc463c246abda077e08187536f212b42ba17
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-01 19:02:20 +0600

    Gray code iterative and recursive

---
+++ LeetCode_July_Challenge_2021/gray_code.cpp
@@ -0,0 +1,42 @@
+//Iterative Approach
+// Time complexity: O(2 ^ n)
+// Space complexity: O(1)
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int>result;
+        int sequenceLength = 1 << n;
+        
+        for(int i=0;i< sequenceLength ; i++){
+            int num= i ^ i >> 1;
+            result.push_back(num);
+        }
+        
+        return result;
+    }
+};
+
+//Iterative Approach
+// Time complexity: O(2 ^ n)
+// Space complexity: O(n)
+class Solution {
+public:
+    int nextNum=0;
+    void grayCodeHelper(vector<int>& result,int n){
+        if(n==0){
+            result.push_back(nextNum);
+            return;
+        }
+        grayCodeHelper(result,n-1);
+        nextNum = nextNum ^ (1 << (n-1));
+        grayCodeHelper(result,n-1);
+        
+    }
+    vector<int> grayCode(int n) {
+        vector<int>result;
+        grayCodeHelper(result,n);
+        
+        return result;
+    }
+};
