89e8b4f3c9445d7088f70a1ce34d965736335b7d
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-02 21:00:35 +0600

    Find k closest element using prority queue

--- LeetCode_July_Challenge_2021/find_k_closest_elements.cpp
+++ LeetCode_July_Challenge_2021/find_k_closest_elements.cpp
@@ -80,3 +80,26 @@ public:
         return vector<int>(A.begin()+left,A.begin()+left+k);
        }
    };
+
+class Solution {
+public:
+
+       vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+            for (int i = 0; i < arr.size(); i++)
+                 pq.push({abs(arr[i] - x), arr[i]});
+        
+            vector<int> ans;
+
+           for (int i = 0; i < k; i++){
+                ans.push_back(pq.top().second);
+                pq.pop(); 
+           }
+
+           sort(ans.begin(), ans.end());
+           return ans;
+    }
+
+};
