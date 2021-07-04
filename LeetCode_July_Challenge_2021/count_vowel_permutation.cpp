d2bd6bd46c9b84ea47b238a1428384743bf19ff4
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-04 22:05:01 +0600

    Count Vowels Permutation bottom up dp solution

--- LeetCode_July_Challenge_2021/count_vowel_permutation.cpp
+++ LeetCode_July_Challenge_2021/count_vowel_permutation.cpp
@@ -25,3 +25,56 @@ public:
         return result;
     }
 };
+
+//Bottom Up DP
+//Time Complexity O(n)
+//Space Comlexity O(n)
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        long a[n],e[n],i[n],o[n],u[n];
+        a[0]=1;
+        e[0]=1;
+        i[0]=1;
+        o[0]=1;
+        u[0]=1;
+        int MOD = 1e9+7;
+        for(int x=1;x<n;x++){
+            a[x]=(e[x-1]+i[x-1]+u[x-1]) % MOD;
+            e[x]=(a[x-1]+i[x-1]) % MOD;
+            i[x]=(e[x-1]+o[x-1])  % MOD;
+            o[x]=(i[x-1])  % MOD;
+            u[x]=(i[x-1]+o[x-1]) % MOD;
+        }
+        
+        long result=(a[n-1]+e[n-1]+i[n-1]+o[n-1]+u[n-1]) % MOD;
+        
+        return result;
+    }
+};
+
+class Solution {
+public:
+    using ll=long long;
+    int mod=1e9+7;
+    int countVowelPermutation(int n) {
+        vector<vector<ll> >dp(n+1,vector<ll>(5));
+        //dp[i][j]-> res for subproblem of length i ending with jth vowel
+        //j=0 for 'a'
+        //j=1 for 'e'
+        //j=2 for 'i'
+        //j=3 for 'o'
+        //j=4 for 'u'
+        dp[1][0]=dp[1][1]=dp[1][2]=dp[1][3]=dp[1][4]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i][0]=((dp[i-1][1]%mod+dp[i-1][2]%mod)%mod+dp[i-1][4]%mod)%mod;
+            dp[i][1]=(dp[i-1][0]%mod+dp[i-1][2]%mod)%mod;
+            dp[i][2]=(dp[i-1][1]%mod+dp[i-1][3]%mod)%mod;
+            dp[i][3]=dp[i-1][2]%mod;
+            dp[i][4]=(dp[i-1][2]%mod+dp[i-1][3]%mod)%mod;
+            
+        }
+        return (dp[n][0]+dp[n][1]+dp[n][2]+dp[n][3]+dp[n][4])%mod;
+    }
+};
