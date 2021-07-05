e5654ba1a81da37dceea26c9e9b5f82daccbadad
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-05 21:32:02 +0600

    Reshape the matrix another way

--- LeetCode_July_Challenge_2021/reshape_the_matrix.cpp
+++ LeetCode_July_Challenge_2021/reshape_the_matrix.cpp
@@ -20,3 +20,27 @@ public:
         return res;
     }
 };
+
+//Time Complexity O(n^2)
+//Space Complexity O(n)
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m=mat.size();
+        int n=mat[0].size();
+        if(m==0 || m*n != r*c) return mat;
+        
+        vector<vector<int>>ans(r,vector<int>(c,0));
+        
+        int x=0,y=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ans[i][j]=mat[x][y++];
+                if(y==n){
+                    y%=n, x++;
+                }
+            }
+        }
+        return ans;
+    }
+};
