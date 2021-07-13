9f2b2fe6da8e3957a5ebf327fdff85252c1c4f15
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-13 23:19:41 +0600

    Find peak elements iterative binary search

--- LeetCode_July_Challenge_2021/find_peak_element.cpp
+++ LeetCode_July_Challenge_2021/find_peak_element.cpp
@@ -33,3 +33,21 @@ public:
         return binarySearch(nums,0,nums.size()-1);
     }
 };
+
+//Iterative Binary Search Approach
+//Time Complexity O(log n)
+//Space Complexity O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid] > nums[mid+1]) 
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+};
