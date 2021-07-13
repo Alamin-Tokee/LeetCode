f66fd5bb7ec700094908603d484e251403c30ec9
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-13 23:18:46 +0600

    Find peak elements recursive binary search

--- LeetCode_July_Challenge_2021/find_peak_element.cpp
+++ LeetCode_July_Challenge_2021/find_peak_element.cpp
@@ -13,3 +13,23 @@ public:
         return nums.size()-1;
     }
 };
+
+
+//Recursive Binary Search Approach
+//Time Complexity O(log n)
+//Space Complexity O(1)
+
+class Solution {
+public:
+    int binarySearch(vector<int>&nums,int l,int r){
+        if(l==r) return l;
+        int mid=(l+r)/2;
+        if(nums[mid] > nums[mid+1]) return binarySearch(nums,l,mid);
+        
+        return binarySearch(nums,mid+1,r);
+    }
+    int findPeakElement(vector<int>& nums) {
+    
+        return binarySearch(nums,0,nums.size()-1);
+    }
+};
