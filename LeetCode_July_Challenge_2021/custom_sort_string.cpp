349b7fddb94a24a413085f86c6971ded4b4241ee
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-14 22:09:58 +0600

    Custom sort string hashing

--- LeetCode_July_Challenge_2021/custom_sort_string.cpp
+++ LeetCode_July_Challenge_2021/custom_sort_string.cpp
@@ -75,3 +75,31 @@ public:
         return str;
     }
 };
+
+
+//Using Hash Table
+//Time complexity O(n)
+//Space complexity O(n)
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        map<char,int>mp;
+        string ans="";
+        for(auto x:str) mp[x]++;
+        
+        for(auto x:order){
+            if(mp.find(x)!=mp.end()){
+                auto temp=mp.find(x);
+                int count=temp->second;
+                string s(count,x);
+                ans+=s;
+                mp.erase(x);
+            }
+        }
+        for(auto x:mp){
+            string s(x.second,x.first);
+            ans+=s;
+        }
+        return ans;
+    }
+};
