6c6252262177857496c37750ffc52aad7bf2879b
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-14 22:08:35 +0600

    Custom sort string quick sort

--- LeetCode_July_Challenge_2021/custom_sort_string.cpp
+++ LeetCode_July_Challenge_2021/custom_sort_string.cpp
@@ -34,3 +34,44 @@ public:
         return str;
     }
 };
+
+
+//Quick sort 
+//Time complexity O(nlogn)
+//Space complexity O(1)
+
+class Solution {
+public:
+    int partition(vector<int>& v,string &str,int l,int r){
+        int i=l-1;
+        int pivot=v[str[r]-'a'];
+        for(int j=l;j<r;j++){
+            if(v[str[j]-'a'] < pivot){
+                i++;
+                swap(str[i],str[j]);
+            }
+        }
+        i++;
+        swap(str[i],str[r]);
+        return i;
+    }
+    void quickSort(vector<int>& v,string &str,int l,int r){
+        if(l>=r) return;
+        
+        int p=partition(v,str,l,r);
+        
+        quickSort(v,str,l,p-1);
+        quickSort(v,str,p+1,r);
+    }
+    string customSortString(string order, string str) {
+        int n=order.size();
+        vector<int>v(26,n);
+        for(int i=0;i<n;i++){
+            v[order[i]-'a']=i;
+        }
+        
+        quickSort(v,str,0,str.size()-1);
+        
+        return str;
+    }
+};
