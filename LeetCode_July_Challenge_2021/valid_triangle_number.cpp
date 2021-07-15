61ca258bba5f673706631dec8fb24c34c879716e
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-15 20:32:20 +0600

    Valid Triangle Number Binary Search

--- LeetCode_July_Challenge_2021/valid_triangle_number.cpp
+++ LeetCode_July_Challenge_2021/valid_triangle_number.cpp
@@ -39,3 +39,34 @@ public:
         return res;
     }
 };
+
+//Binary Search Approach
+//Time Complexity O(n^2logn)
+//Space Complexity O(logn)
+class Solution {
+public:
+    int binarySearch(vector<int>& nums,int l,int r,int x){
+        while(r >= l && r < nums.size()){
+            int mid=(l+r)/2;
+            if(nums[mid] >= x){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+    int triangleNumber(vector<int>& nums) {
+
+        int count = 0;
+        sort(begin(nums),end(nums));
+        for(int i=0;i<nums.size()-2;i++){
+            int k=i+2;
+            for(int j=i+1;j<nums.size()-1 && nums[i]!=0 ; j++){
+                k=binarySearch(nums,k,nums.size()-1,nums[i]+nums[j]);
+                count += k-j-1;
+            }
+        }
+        return count;
+    }
+};
