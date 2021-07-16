28813eb82976924bc2bec1dae88e6b929d926446
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-16 20:25:22 +0600

    4 sum added another approach

--- LeetCode_July_Challenge_2021/4sum.cpp
+++ LeetCode_July_Challenge_2021/4sum.cpp
@@ -30,3 +30,47 @@ public:
         return ans;
     }
 };
+
+
+
+//Using hash and set
+//Time complexity O(n^3)
+//Space compleixty O(n)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        if(nums.empty()) return ans;
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int target2=target-(nums[i]+nums[j]);
+                int left=j+1;
+                int right=n-1;
+                while(left < right){
+                    int two_sum=nums[left]+nums[right];
+                    if(two_sum < target2) left++;
+                    else if(two_sum > target2) right--;
+                    else{
+                        vector<int>res(4,0);
+                        res[0]=nums[i];
+                        res[1]=nums[j];
+                        res[2]=nums[left];
+                        res[3]=nums[right];
+                        ans.push_back(res);
+                        
+                        while(left < right && nums[left]==res[2]) left++;
+                        
+                        while(left < right && nums[right]==res[3]) right--;
+                    }
+                }
+                while(j+1 < n && nums[j+1]==nums[j]) ++j;
+            }
+            while(i+1 < n && nums[i+1]==nums[i]) ++i;
+        }
+        return ans;
+    }
+};
