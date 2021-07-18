6e3802ed6ba7cbbea1177c98da22994806aa5f90
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-18 23:29:15 +0600

    Reverse nodes in k group iterative

--- LeetCode_July_Challenge_2021/reverse_node_in_k_group.cpp
+++ LeetCode_July_Challenge_2021/reverse_node_in_k_group.cpp
@@ -55,3 +55,34 @@ public:
         return c;
     }
 };
+
+
+//Iterative approach
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int n = 0;
+        ListNode *curr=head,*prev,*next_node,*start_prev,*start_curr;
+        while(curr!=NULL){
+            n++,curr=curr->next;
+        }
+        n=n/k;curr=head;
+        for(int i=0;i<n;i++){
+            prev=NULL;
+            start_curr = curr;
+            for(int j=0;j<k;j++){
+                next_node = curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=next_node;
+            }
+            if(i==0) head=prev;
+            if(i > 0) start_prev->next= prev;
+            
+            start_prev=start_curr;
+        }
+        if(start_curr) start_curr->next = curr;
+        
+        return head;
+    }
+};
