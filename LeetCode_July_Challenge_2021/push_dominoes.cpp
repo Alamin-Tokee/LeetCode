82838d2de1f9706c5c2dc01a631925a6d2859784
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-22 23:54:58 +0600

    Push dominoes another solution

--- LeetCode_July_Challenge_2021/push_dominoes.cpp
+++ LeetCode_July_Challenge_2021/push_dominoes.cpp
@@ -64,3 +64,76 @@ public:
         return finalResult;
     }
 };
+
+
+//Sollution
+class Solution {
+public:
+    string pushDominoes(string s) {
+        if(s.size() == 1)
+            return s;
+        string fans = "";
+        while(1){
+            string ans = "";
+            for(int i = 0; i < s.size(); i++){
+                if(i == 0){
+                    if(s[i] == '.' and s[i + 1] == 'L') ans += s[i + 1];
+                    else ans += s[i];
+                }
+                else if(i == s.size() - 1){
+                    if(s[i] == '.' and s[i - 1] == 'R') ans += s[i - 1];
+                    else ans += s[i];
+                }
+                else{
+                    if(s[i] == '.' and s[i - 1] == 'R' and s[i + 1] != 'L') ans += s[i - 1];
+                    else if(s[i] == '.' and s[i + 1] == 'L' and s[i - 1] != 'R') ans += s[i + 1];
+                    else ans += s[i];
+                }
+            }
+            s = ans;
+            if(s == fans) break;
+            else fans = s; 
+        }
+        return fans;
+    }
+};
+//Solution II:
+
+#define ll long long int
+class Solution {
+public:
+    string pushDominoes(string s) {
+        ll n = s.size();
+        if(n == 1)
+            return s;
+        vector<ll> r(n), l(n);
+        for(int i = 0; i < n; i++){
+            if(s[i] == 'R') r[i] = 1;
+            else if(s[i] == '.' and i > 0){
+                if(r[i - 1] != INT_MAX) r[i] = r[i - 1] + 1;
+                else r[i] = INT_MAX;
+            }
+            else r[i] = INT_MAX;     
+        }
+        
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == 'L') l[i] = 1;
+            else if(s[i] == '.' and i < n - 1){
+                if(l[i + 1] != INT_MAX) l[i] = l[i + 1] + 1;
+                else l[i] = INT_MAX;
+            }
+            else l[i] = INT_MAX;
+        }
+        
+        string ans = "";
+        for(int i = 0; i < n; i++){
+            if(s[i] == '.'){
+                if(l[i] == r[i]) ans += '.';
+                else if(l[i] < r[i]) ans += 'L';
+                else if(l[i] > r[i]) ans += 'R';
+            }
+            else ans += s[i];
+        }
+        return ans;
+    }
+};
