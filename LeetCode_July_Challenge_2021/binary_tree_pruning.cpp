939d1255a4bc8dadd3a98d6e7e0c5a5973767eb2
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-23 20:56:39 +0600

    Binary Tree Pruning updated

--- LeetCode_July_Challenge_2021/binary_tree_pruning.cpp
+++ LeetCode_July_Challenge_2021/binary_tree_pruning.cpp
@@ -45,3 +45,22 @@ public:
         return (hasOne(root)) ? root : NULL;
     }
 };      
+
+
+//Optimal Approach
+//Time Complexity O(n)
+//Space Complexity O(1)
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root) return NULL;
+        
+        root->left=pruneTree(root->left);
+        
+        root->right=pruneTree(root->right);
+        
+        if(!root->left && !root->right && root->val == 0) return NULL;
+        
+        return root;
+    }
+};
