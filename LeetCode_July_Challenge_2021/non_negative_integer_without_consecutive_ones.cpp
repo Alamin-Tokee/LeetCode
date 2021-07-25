d7e4a866f2008b57d0ec2809eb240425bc53ba47
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-25 22:26:13 +0600

    Non-negative Integers without Consecutive Ones Dp Solution

--- LeetCode_July_Challenge_2021/non_negative_integer_without_consecutive_ones.cpp
+++ LeetCode_July_Challenge_2021/non_negative_integer_without_consecutive_ones.cpp
@@ -41,3 +41,29 @@ public:
         return 1+countValidInteger(1,n);
     }
 };
+
+//Dp Solution
+//Time Complexity O(log2(max_int)==32)
+//Space Complexity O(log2(max_int)==32)
+class Solution {
+public:
+    int findIntegers(int num) {
+        int f[32];
+        f[0] = 1;
+        f[1] = 2;
+        for (int i = 2; i < 32; ++i)
+            f[i] = f[i-1]+f[i-2];
+        int ans = 0, k = 30, pre_bit = 0;
+        while (k >= 0) {
+            if (num&(1<<k)) {
+                ans += f[k];
+                if (pre_bit) return ans;
+                pre_bit = 1;
+            }
+            else
+                pre_bit = 0;
+            --k;
+        }
+        return ans+1;
+    }
+};
