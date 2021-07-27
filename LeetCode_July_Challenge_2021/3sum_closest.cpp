a2a2f2bc1179999c64ab1e245dcf0fd6d1204f03
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-27 23:14:40 +0600

    3Sum Closest Updated

--- LeetCode_July_Challenge_2021/3sum_closest.cpp
+++ LeetCode_July_Challenge_2021/3sum_closest.cpp
@@ -55,7 +55,46 @@ public:
          return minRes;
      } 
 };
-    
+
+//Binary Search
+//O(n^2 logn)
+//Space Complexity O(n)
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int result=0;
+        int distance=INT_MAX;
+        for(int i=0; i<nums.size()-2; i++){
+            for(int j=i+1; j<nums.size()-1; j++){
+                int tong=nums[i]+nums[j];
+                int end=nums.size()-1;
+                int start=j+1;
+                while(start<=end){
+                    int mid=(start+end)/2;
+                    int check=tong+nums[mid];
+                    if(check<target){
+                        start=mid+1;
+                        if(distance>abs(check-target)){
+                            distance=abs(check-target);
+                            result=check;
+                        }
+                    }
+                    else{
+                        end=mid-1;
+                        if(distance>abs(check-target)){
+                            distance=abs(check-target);
+                            result=check;
+                        }
+                    }
+                }
+                
+            }
+        }
+        return result;
+    }
+};    
 
 
-    
+
