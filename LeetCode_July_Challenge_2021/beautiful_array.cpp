1ca11b42c3c2e4321cd0303abb2c8738ce3ab239
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-28 22:20:21 +0600

    Beautiful Array Recursive Added

--- LeetCode_July_Challenge_2021/beautiful_array.cpp
+++ LeetCode_July_Challenge_2021/beautiful_array.cpp
@@ -27,3 +27,32 @@ public:
         return ret;
     }
 };
+
+
+//Recursive Solution
+//Time Complexity O(NlogN)
+//Space Complexity O(NlogN)
+
+class Solution {
+public:
+    unordered_map<int,vector<int>> mp;
+    vector<int> beautifulArray(int n) {
+        if(mp.find(n)!=mp.end()) return mp[n];
+        
+        vector<int> ans(n,0);
+        if(n == 1) ans[0] = 1;
+        else{
+            int index = 0;
+            for(int x : beautifulArray((n+1)/2)){
+                cout<< x <<endl;
+                ans[index++]=2*x-1;
+            }
+            
+            for(int x : beautifulArray(n/2))
+                ans[index++]=2*x;
+        }
+        mp[n]=ans;
+        
+        return ans;
+    }
+};
