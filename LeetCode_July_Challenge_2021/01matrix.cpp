262d72b3303a9bd993f73d05bf23ceb30fc07acb
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-29 19:20:24 +0600

    01 matrix updated

--- LeetCode_July_Challenge_2021/01matrix.cpp
+++ LeetCode_July_Challenge_2021/01matrix.cpp
@@ -32,3 +32,34 @@ public:
         return dist;
     }
 };
+
+//BFS Solution
+//Time Complexity O(r*c)
+//Space Complexity O(1)
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,1e4));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    dp[i][j]=mat[i][j];
+                }else{
+                    if(i > 0) dp[i][j]=min(dp[i][j],1+dp[i-1][j]);
+                    if(j > 0) dp[i][j]=min(dp[i][j],1+dp[i][j-1]);
+                }
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+   
+                if(i+1 < n) dp[i][j]=min(dp[i][j],1+dp[i+1][j]);
+                if(j+1 < m) dp[i][j]=min(dp[i][j],1+dp[i][j+1]);
+
+            }
+        }
+        return dp;
+    }
+};
