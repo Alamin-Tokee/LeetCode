283334034e045d1558bd96c4de7f9ada5e7ad05a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-30 21:34:23 +0600

    Max sum pairs

---
+++ LeetCode_July_Challenge_2021/map_sum_pairs.cpp
@@ -0,0 +1,38 @@
+//Brute Force Solution
+//Time Complexity O(n)
+//Space Complexity O(n)
+class MapSum {
+public:
+    /** Initialize your data structure here. */
+    unordered_map<string,int> mp;
+    MapSum() {
+        mp.clear();
+    }
+    
+    void insert(string key, int val) {
+        mp[key]=val;
+    }
+    
+    int sum(string prefix) {
+        int n=prefix.size();
+        int sum=0;
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->first.substr(0,n)==prefix){
+                sum+=it->second;
+            }
+        }
+        return sum;
+    }
+};
+
+//     int sum(string prefix) {
+//         int sum = 0, n = prefix.size();
+//         for (auto it = mp.lower_bound(prefix); it != mp.end() && it->first.substr(0, n) == prefix; it++) 
+//             sum += it->second;
+//         return sum;
+//     }
+
+//         auto res = mismatch(prefix.begin(), prefix.end(), it.first.begin());
+//         if( res.first == prefix.end() ){
+//             total += it.second;
+//         }
