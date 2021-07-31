c3f1e26444420699654fd3c2dfff6efff273bb48
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-07-31 23:03:07 +0600

    Trapping Rain Water Stack Solution

--- LeetCode_July_Challenge_2021/trapping_rain_water.cpp
+++ LeetCode_July_Challenge_2021/trapping_rain_water.cpp
@@ -22,3 +22,31 @@ public:
         return ans;
     }
 };
+
+//Stack Solution
+//Time Complexity O(n)
+//Space Complexity O(n)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int>st;
+        int ans = 0, index = 0;
+        while(index < height.size()){
+            while(!st.empty() && height[st.top()]<height[index]){
+                int top = st.top();
+                st.pop();
+                // agar left mei koi bdi nhi h, toh paani nhi store ho skta naa, isliye break
+                if(st.empty())
+                    break;
+                // jo two unchi building h current building se, unke beech ka distance 
+                int distance = index - st.top() - 1;
+                // height nikal rhe h kitni increase ho skti h 
+                int add_height = min(height[st.top()],height[index]) - height[top];
+                // mutiple building jo current building se choti h uske left aur right mei uske liye mutiply krenge                        distance se 
+                ans += add_height * distance;
+            }
+            st.push(index++);
+        }
+        return ans;
+    }
+};
