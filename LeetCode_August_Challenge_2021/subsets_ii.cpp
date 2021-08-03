5546577dfe38f9352a708ab316a24cd98399863d
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-03 22:40:32 +0600

    Subsets Recusive Backtracking

--- LeetCode_August_Challenge_2021/subsets_ii.cpp
+++ LeetCode_August_Challenge_2021/subsets_ii.cpp
@@ -25,7 +25,7 @@ public:
     }
 };
 
-//Iterative Solution
+//Iterative Solution(bit masking)
 //Time Complexity O(n^2)
 //Space Complexity (n)
 
@@ -73,3 +73,83 @@ public:
     }
 };
 
+//Recusive Solution(BackTracking)
+//Time Complexity O(n*2^n)
+//Space Complexity (n)
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    void backtrack(vector<int>& nums, vector<int> temp, int idx) {
+        result.push_back(temp);
+        if(idx >= nums.size())
+            return;
+        
+        for(int i = idx; i<nums.size(); i++) {
+            if(i > idx && nums[i] == nums[i-1]) continue; //avoid duplicates
+            
+            temp.push_back(nums[i]);     //include
+            backtrack(nums, temp, i+1);  //explore
+            temp.pop_back();             //exclude
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        result.clear();
+        
+        vector<int> temp;
+        sort(begin(nums), end(nums)); //will help avoiding duplicates
+        backtrack(nums, temp, 0);
+        
+        return result;
+    }
+};
+
+class Solution {
+public:
+    void subsetUtil(vector<int> A, vector<vector<int>> &ans, vector<int> temp, int index,
+                set<vector<int>> &s){
+    if(s.find(temp) == s.end()){
+        ans.push_back(temp);
+        s.insert(temp);
+    }   
+    
+    for(int i=index;i<A.size();++i){
+        temp.push_back(A[i]);
+        
+        subsetUtil(A, ans, temp, i+1, s);
+        
+        temp.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& A) {
+        sort(A.begin(),A.end());
+    
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        vector<int> temp;
+
+        subsetUtil(A,ans,temp,0,s);
+
+        return ans;
+    }
+};
+
+void helper(vector<int>& nums,vector<vector<int>>& res,vector<int> temp,int idx,int flag){
+        
+        if(idx==nums.size()){
+            res.push_back(temp);
+            temp.clear();
+            return;
+        }
+        if(flag==0 || nums[idx]!=nums[idx-1])
+        helper(nums,res,temp,idx+1,0);
+        temp.push_back(nums[idx]);
+        helper(nums,res,temp,idx+1,1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
+        vector<int>temp;
+        helper(nums,res,temp,0,0);
+        return res;
+    }
