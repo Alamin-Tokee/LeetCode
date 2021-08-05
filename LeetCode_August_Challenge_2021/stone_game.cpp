66b65c36f31901ffb57b18f6af265f6d49de1587
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-05 14:21:38 +0600

    Stone game dynamic programming tabulization

--- LeetCode_August_Challenge_2021/stone_game.cpp
+++ LeetCode_August_Challenge_2021/stone_game.cpp
@@ -8,7 +8,7 @@ public:
     }
 };
 
-//Math Solution
+//Memoization Solution
 //Time Complexity O(n*n)
 //Space Complexity O(n*n)
 class Solution {
@@ -50,5 +50,32 @@ public:
     }
     bool stoneGame(vector<int>& piles) {
         return dfs(piles, 0) * 2 > accumulate(begin(piles), end(piles), 0);
+    }
+};
+
+
+//Tabulization Solution
+//Time Complexity O(n*n)
+//Space Complexity O(n*n)
+class Solution {
+public:
+    bool stoneGame(vector<int>& nums) {
+          int n = nums.size();
+        int dp[n][n];
+        
+        for(int gap = 0; gap < n; gap++) {
+            for(int i = 0, j = gap; j < n; i++, j++) {
+                if(gap == 0) dp[i][j] = nums[i];
+                else if(gap == 1) dp[i][j] = max(nums[i], nums[j]);
+                else {
+                    int val1 = nums[i] + min(dp[i + 2][j], dp[i + 1][j - 1]);
+                    int val2 = nums[j] + min(dp[i][j - 2], dp[i + 1][j - 1]);
+                    dp[i][j] = max(val1, val2);
+                }
+            }
+        }
+        
+      
+        return dp[0][n - 1] ;
     }
 };
