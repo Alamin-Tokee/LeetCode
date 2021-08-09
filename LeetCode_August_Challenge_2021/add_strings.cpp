d5264974b8e57b4df8ecc1d27ea275687cea03ca
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-09 20:25:18 +0600

    Add strings

--- LeetCode_August_Challenge_2021/add_strings.cpp
+++ LeetCode_August_Challenge_2021/add_strings.cpp
@@ -27,3 +27,34 @@ public:
         return ans;
     }
 };
+
+
+// First Solution
+// Time Complexity O(m+n)
+// Space Complexity O(1)
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        reverse(num1.begin(),num1.end());
+        reverse(num2.begin(),num2.end());
+        string ans="";
+        int carry=0;
+        
+        if(num1.size() > num2.size()) swap(num1,num2);
+        while(num1.size() < num2.size()){
+            num1+='0';
+        }
+        for(int i=0;i<num1.length();i++){
+            int curr=num1[i]-'0'+num2[i]-'0'+carry;
+            ans+=(curr%10)+'0';
+            carry=curr/10;
+        }
+        
+        if(carry){
+            ans+=(carry+'0');
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
