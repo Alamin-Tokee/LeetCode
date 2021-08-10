736313d1748b91646625a19ca1698aa80722f23a
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-10 22:33:39 +0600

    Flip string to monotone increasing

--- LeetCode_August_Challenge_2021/flip_string_to_monotone_increasing.cpp
+++ LeetCode_August_Challenge_2021/flip_string_to_monotone_increasing.cpp
@@ -18,3 +18,34 @@ public:
         return zeroCount;
     }
 };
+
+
+//Second Approach(DP)
+//Time Complexity O(n)
+//Space Complexity O(b)
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        //Keep track of left one's and right Zeroes for each element.
+        // Now for every element, total flips are (leftOnes + rightZeroes)
+        // i.e, leftOnes are flipped from 1 to 0, rightZeroes are flipped from 0 to 1
+        //Don't require to bother about current element
+        // Find the minimum.
+        int n=s.size();
+        vector<int>leftOnes(n, 0);
+        vector<int>rightZeroes(n,0);
+        for(int i=1;i<n;i++)
+            leftOnes[i]=leftOnes[i-1]+((s[i-1]=='1')?1:0);
+        for(int i=n-2;i>=0;i--) {
+            rightZeroes[i]=rightZeroes[i+1]+((s[i+1]=='0')?1:0);
+        }
+        for(int i=0;i<n;i++){
+            cout<< leftOnes[i] << " ";
+        }
+        int res=INT_MAX;
+        for(int i=0;i<n;i++) {
+            res=min(res, leftOnes[i]+rightZeroes[i]);
+        }
+        return res;
+    }
+};
