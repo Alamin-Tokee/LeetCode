8811b39e5eb3ca1b514b45c92c3fca53a9810be5
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-12 18:00:26 +0600

    Group anagrams

--- LeetCode_August_Challenge_2021/group_anagrams.cpp
+++ LeetCode_August_Challenge_2021/group_anagrams.cpp
@@ -52,3 +52,35 @@ public:
     }
 };
 
+
+//Time Complexity (m*nlogn)
+//Space Complexity O(n*m)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<pair<string,int>>v;
+        vector<string>sub;
+        vector<vector<string>>vv;
+        string x;
+        for(int i=0;i<strs.size();i++)
+        {
+          x=strs[i];
+          sort(x.begin(),x.end()); 
+          v.push_back({x,i});  
+        }
+        sort(v.begin(),v.end());
+      
+        for(int i=0;i<v.size()-1;i++)
+        {
+            sub.push_back(strs[v[i].second]);
+            if(v[i].first!=v[i+1].first)
+            {
+                vv.push_back(sub);
+                sub.clear();
+            }
+        }
+        sub.push_back(strs[v[v.size()-1].second]);
+        vv.push_back(sub);
+        return vv;
+    }
+};
