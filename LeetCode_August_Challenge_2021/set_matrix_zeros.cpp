fd3fd9ffb73ef5d0922bc4e068ded069c5c73bd8
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-13 16:32:20 +0600

    Set matrix zeros anaother solution

--- LeetCode_August_Challenge_2021/set_matrix_zeros.cpp
+++ LeetCode_August_Challenge_2021/set_matrix_zeros.cpp
@@ -33,3 +33,61 @@ public:
     //     for(int i=0;i<n1;i++)
     //         matrix[i][j]=0;
     // }
+
+//Time Complexity O(m*n)
+//Space Complexity O(n+m)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        map<int,int>r,c;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==0){
+                    r[i]=2;
+                    c[j]=2;
+                }
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(r[i]==2 || c[j]==2){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
+
+//Time Complexity O(m*n)
+//Space Complexity O(n+m)
+class Solution {
+public:
+    void rowZero(vector<vector<int>>& matrix,int i){
+        for(int j=0;j<matrix[0].size();j++){
+            matrix[i][j]=0;
+        }
+    }
+    void colZero(vector<vector<int>>& matrix,int j){
+        for(int i=0;i<matrix.size();i++){
+            matrix[i][j]=0;
+        }
+    }
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==0){
+                    v.push_back({i,j});
+                }
+            }
+        }
+        for(int i=0;i<v.size();i++){
+            int x=v[i].first;
+            int y=v[i].second;
+            rowZero(matrix,x);
+            colZero(matrix,y);
+        }
+    }
+};
+
+
