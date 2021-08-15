7f5cdbadb87342ae64213cc26f56452b7ad38355
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-15 17:10:10 +0600

    Minimum window substring tricks

--- LeetCode_August_Challenge_2021/minimum_window_substring.cpp
+++ LeetCode_August_Challenge_2021/minimum_window_substring.cpp
@@ -63,3 +63,29 @@ public:
         return len==INT_MAX ? "" : s.substr(begin,len);
     }
 };
+
+//Time Complexity O(m+n)
+//Space Complexity O(m+n)
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int>hist(128,0);
+        
+        for(char c:t) hist[c]++;
+        
+        int remaining = t.length();
+        int left=0,right=0,minStart=0,minLen=numeric_limits<int>::max();
+        while(right < s.length()){
+            if(--hist[s[right++]] >=0) remaining--;
+            while(remaining == 0){
+                if(right-left < minLen){    
+                    minLen=right-left;
+                    minStart=left;
+                }
+                if(++hist[s[left++]] > 0) remaining++;
+            }
+        }
+        return minLen < numeric_limits<int>::max() ? s.substr(minStart,minLen) : "";
+    }
+};
