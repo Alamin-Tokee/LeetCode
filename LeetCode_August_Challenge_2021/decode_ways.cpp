882be56269feedca0f5b69cfe742fbbe09323b93
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-18 19:07:58 +0600

    Decode Ways

--- LeetCode_August_Challenge_2021/decode_ways.cpp
+++ LeetCode_August_Challenge_2021/decode_ways.cpp
@@ -50,3 +50,41 @@ public:
         return memo[i] = ans;
     }
 };
+
+
+class Solution { // 0 ms, faster than 100.00%
+public:
+    int numDecodings(const string& s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] != '0') // Single digit
+                dp[i] += dp[i+1];
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
+    }
+};
+
+//Bottom Up
+//TC O(n)
+//SC O(n)
+
+class Solution { // 0 ms, faster than 100.00%
+public:
+    int numDecodings(const string& s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] != '0') // Single digit
+                dp[i] += dp[i+1];
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
+    }
+};
+
