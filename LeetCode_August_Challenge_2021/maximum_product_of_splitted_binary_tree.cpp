834ef6eeaf9d6b5fb815f381ba9ddeca88a74acd
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-19 15:19:11 +0600

    Maximum product of split binary tree

--- LeetCode_August_Challenge_2021/maximum_product_of_splitted_binary_tree.cpp
+++ LeetCode_August_Challenge_2021/maximum_product_of_splitted_binary_tree.cpp
@@ -32,3 +32,53 @@ public:
 
 //Time Complexity O(n)
 //Space Complexity O(n)
+
+class Solution {
+public:
+    int mod=1e9+7;
+    int tot=0;
+    int getSum(TreeNode* root){
+        if(!root) return 0;
+        if(!root->left && !root->right) return root->val;
+        int left=getSum(root->left);
+        int right=getSum(root->right);
+        
+        root->val+=left+right;
+        
+        return root->val;
+    } 
+    
+    void getMaxProduct(TreeNode* root,long int& res){
+        if(!root) return;
+        
+        if(root->left){
+            long int sum1=root->left->val;
+            long int sum2=tot-sum1;
+            long int curr=sum1*sum2;
+            
+            if(res < curr) 
+                res=curr;
+        }
+        if(root->right){
+            long int sum1=root->right->val;
+            long int sum2=tot-sum1;
+            long int curr=sum1*sum2;
+            
+            if(res < curr) 
+                res=curr;
+        }
+        getMaxProduct(root->left,res);
+        getMaxProduct(root->right,res);
+    }
+    
+    int maxProduct(TreeNode* root) {
+        if(!root) return 0;
+        
+        tot=getSum(root);
+        
+        long int res = INT_MIN;
+        getMaxProduct(root,res);
+        
+        return res % mod;
+    }
+};
