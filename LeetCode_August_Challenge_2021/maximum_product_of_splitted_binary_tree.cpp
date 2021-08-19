3033a2bc9b136f41df8a59488ea9e971de7323ab
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-19 15:20:07 +0600

    Maximum product of split binary tree

--- LeetCode_August_Challenge_2021/maximum_product_of_splitted_binary_tree.cpp
+++ LeetCode_August_Challenge_2021/maximum_product_of_splitted_binary_tree.cpp
@@ -82,3 +82,82 @@ public:
         return res % mod;
     }
 };
+
+//Time Complexity O(n)
+//Space Complexity O(n)
+class Solution {
+public:
+    int mod=1e9+7;
+    int getSum(TreeNode* root,vector<long long>&ans){
+        if(!root) return 0;
+    
+        int l=getSum(root->left,ans);
+        int r=getSum(root->right,ans);
+        int sum=l+r+root->val;
+        ans.push_back(sum);
+        
+        return sum;
+    }
+    
+    int maxProduct(TreeNode* root) {
+        if(!root) return 0;
+        vector<long long>ans;
+        getSum(root,ans);
+        
+        long long res=INT_MIN;
+        for(int i=0;i<ans.size()-1;i++){
+            res=max(res,ans[i]*(ans[ans.size()-1]-ans[i]));
+        }
+        
+        return (res % mod);
+    }
+};
+
+//Time Complexity O(n)
+//Space Complexity O(n)
+class Solution {
+public:
+    const long long mod=1e9+7;
+    long long total;
+    long long ans;
+    unordered_map<TreeNode*,long long>mp;
+    long long getSum(TreeNode* root){
+        if(!root) return 0;
+        
+        // long long curr = 0LL;
+        // curr += root->val;
+        // if(root->left)curr = curr + getSum(root->left);
+        // if(root->right)curr = curr + getSum(root->right);
+        
+        long long l=getSum(root->left);
+        long long r=getSum(root->right);
+        long long curr=l+r+root->val;
+        mp[root]=curr;
+        
+        return curr;
+    }
+    void getMaxProduct(TreeNode* root){
+        if(!root) return;
+        long long l,r;
+        
+        r=mp[root];
+        l=total-r;
+        ans=max(ans,r*l);
+        
+        
+        getMaxProduct(root->left);
+        getMaxProduct(root->right);
+        return;
+    }
+    int maxProduct(TreeNode* root) {
+        if(!root) return 0;
+        ios_base::sync_with_stdio(0);cin.tie(0);
+        total=0ll;
+        ans=0ll;
+        total=getSum(root);
+        
+        getMaxProduct(root);
+        
+        return ans % mod;
+    }
+};
