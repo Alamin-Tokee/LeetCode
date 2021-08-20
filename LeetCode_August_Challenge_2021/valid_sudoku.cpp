e3f2ad86d614a1ee202007833e09f111f9ee8812
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-20 17:54:31 +0600

    Valid Sudoku

--- LeetCode_August_Challenge_2021/valid_sudoku.cpp
+++ LeetCode_August_Challenge_2021/valid_sudoku.cpp
@@ -1,5 +1,5 @@
 //Time Complexity O(9*9)
-//Space Complexity(9*9)
+//Space Complexity O(9*9)
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -49,6 +49,40 @@ public:
                 }
             }
         }
+        return true;
+    }
+};
+
+
+//Time Complexity O(9*9)
+//Space Complexity O(1)
+class Solution {
+public:
+    bool isCheck(int r,int c,vector<vector<char>>& board){
+        int val=(r/3)*3+c/3;
+        for(int i=0;i<9;i++){
+            if(board[i][c]==board[r][c] && (r!=i)) return false;
+            if(board[r][i]==board[r][c] && (i!=c)) return false;
+        }
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int idx = (i/3)*3+j/3;
+                if(idx==val && board[i][j]==board[r][c] && !(r==i && c==j)) return false;
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for(int i=0;i<9;++i){
+            for(int j=0;j<9;++j){
+                if(board[i][j]=='.') continue;
+                
+                if(isCheck(i,j,board)==false) return false;
+            }
+        }
+        
         return true;
     }
 };
