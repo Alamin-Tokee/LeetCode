6998c7cb106ef1f0702b1e8295b81a5a8dd9d364
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-25 22:21:51 +0600

    Sum of sqaure number

--- LeetCode_August_Challenge_2021/sum_of_square_number.cpp
+++ LeetCode_August_Challenge_2021/sum_of_square_number.cpp
@@ -41,3 +41,17 @@ public:
         return false ;
     }
 };
+
+//TC O(n)
+//SC O(n)
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        unordered_map<int,int> m;
+        for(int i = 0;i <= 46340; i++){
+            m[(i*i)]++;
+            if(m.find(c - (i*i))!=m.end())return true;
+        }
+        return false;        
+    }
+};
