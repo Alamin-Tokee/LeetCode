9a393cd51437a98f7470188d97cd5e96f21b1502
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-27 20:23:06 +0600

    Verify Preorder Serialization of a Binary Tree

--- LeetCode_August_Challenge_2021/verify_preorder_serialization_of_binary_tree.cpp
+++ LeetCode_August_Challenge_2021/verify_preorder_serialization_of_binary_tree.cpp
@@ -15,3 +15,43 @@ public:
         return node==0;
     }
 };
+
+//Second Approach
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        int a=0,b=0;
+        for(int i=0;i<preorder.length();i++){
+            if(b>a) return false;
+            
+            if(preorder[i]=='#'){
+                b++;
+            }
+            else{
+                a++;
+            }
+            
+            while(i<preorder.length() && preorder[i]!=',') i++;
+        }
+        return a==b-1;
+    }
+};
+
+//Third Approach
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        int node=1;
+        for(int i=0;i<preorder.length();i++){
+            if(preorder[i]==',') continue;
+            node--;
+            if(node < 0) return false;
+            if(preorder[i]!='#'){
+                node+=2;
+                while(i < preorder.length() && preorder[i]>='0' && preorder[i]<='9') i++;
+            }
+        }
+        
+        return node==0;
+    }
+};
