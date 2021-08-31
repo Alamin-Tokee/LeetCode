bfd516803352dd6185f515856a0ed01dceb75543
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-08-31 16:58:07 +0600

    Find minimum rotated sorted array tricks sol

--- LeetCode_August_Challenge_2021/find_minimum_in_rotated_sorted_array.cpp
+++ LeetCode_August_Challenge_2021/find_minimum_in_rotated_sorted_array.cpp
@@ -16,3 +16,22 @@ public:
         return nums[lo];
     }
 };
+
+//Time Complexity O(logn)
+//Space Complexity O(1)
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo=0, hi=nums.size()-1;
+        while(lo < hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[lo] < nums[hi]) return nums[lo];
+            if(nums[mid] < nums[hi]){
+                hi=mid;
+            }else{
+                lo=mid+1;
+            }
+        }
+        return nums[lo];
+    }
+};
