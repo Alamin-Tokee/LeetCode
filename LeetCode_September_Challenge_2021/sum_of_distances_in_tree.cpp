a5b8f71c016195adfade4750f09e7c957e1d6c67
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-05 23:26:37 +0600

    Sum of distance in tree

--- LeetCode_September_Challenge_2021/sum_of_distances_in_tree.cpp
+++ LeetCode_September_Challenge_2021/sum_of_distances_in_tree.cpp
@@ -1,9 +1,47 @@
-
-
+// TIME COMPLEXITY:- O(N)
+// SPACE COMPLEXITY:- O(N)
 class Solution {
 public:
-    // TIME COMPLEXITY:- O(N)
-    // SPACE COMPLEXITY:- O(N)
+    vector<unordered_set<int>> tree;
+    vector<int> v, count;
+    // Counts the path to each node and sums them for every particular node
+    void func1(int root, int x) {
+        for(auto i: tree[root]) {
+            if(i == x) continue;
+            func1(i, root);
+            count[root] += count[i];
+            v[root] += v[i] + count[i];
+        }
+    }
+    // Parent node count - count of closer nodes + count of farther nodes
+    void func2(int root, int x) {
+        for(auto i: tree[root]) {
+            if(i == x) continue;
+            count[root] += count[i];
+            v[i] = v[root] - count[i] + count.size() - count[i];
+            func2(i, root);
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& a) {
+        tree.resize(n);
+        v.assign(n,0);
+        count.assign(n,1);
+        for(auto i: a) {
+            tree[i[0]].insert(i[1]);
+            tree[i[1]].insert(i[0]);
+        }
+        func1(0, -1);
+        func2(0, -1);
+        return v;
+    }
+};
+
+
+
+// TIME COMPLEXITY:- O(N)
+// SPACE COMPLEXITY:- O(N)
+class Solution {
+public:
     int dfs(int v,int par,vector<int> adj[],vector<int>& subtree){
         int curr = 0;
         for(auto u:adj[v]){
