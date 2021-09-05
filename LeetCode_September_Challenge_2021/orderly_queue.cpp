d8da81a68fd708363436c0c26d8644c64be8e8ca
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-06 01:07:44 +0600

    Orderly queue

--- LeetCode_September_Challenge_2021/orderly_queue.cpp
+++ LeetCode_September_Challenge_2021/orderly_queue.cpp
@@ -15,3 +15,26 @@ public:
         return s;
     }
 };
+
+//Time Complexity O(n)
+//Space Complexity O(1)
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k==1){
+            auto minchar=min_element(s.begin(),s.end());
+            string ans=s;
+            for(int i=0;i<s.length();i++){
+                if(s[i]==*minchar){
+                    string suffix=string(s.begin()+i,s.end());
+                    string temp=suffix+string(s.begin(),s.begin()+i);
+                    ans=min(ans,temp); 
+                }
+            }
+            return ans;
+        }
+        sort(s.begin(),s.end());
+        
+        return s;
+    }
+};
