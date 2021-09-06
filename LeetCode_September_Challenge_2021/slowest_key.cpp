da866011bf5b5e538662f528c138aeef481aba31
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-06 16:38:25 +0600

    Slowest key

--- LeetCode_September_Challenge_2021/slowest_key.cpp
+++ LeetCode_September_Challenge_2021/slowest_key.cpp
@@ -24,3 +24,25 @@ public:
         return st;
     }
 };
+
+//Time Complexity O(n+m)
+//Space Complexity (n) (linear space)
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int durationArray[26]={0};
+        durationArray[keysPressed[0]-'a']=releaseTimes[0];
+        for(int i=1;i<releaseTimes.size();i++){
+            int currentDuration=releaseTimes[i]-releaseTimes[i-1];
+            char currentKey=keysPressed[i];
+            durationArray[currentKey-'a']=max(durationArray[currentKey-'a'],currentDuration);
+        }
+        int slowKey = 25;
+        for(int currKey=24;currKey>=0;currKey--){
+            if(durationArray[currKey] > durationArray[slowKey]){
+                slowKey=currKey;
+            }
+        }
+        return slowKey+'a';
+    }
+};
