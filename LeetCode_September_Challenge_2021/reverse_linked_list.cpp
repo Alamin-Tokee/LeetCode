2cfe0cc0bfb5c784aed016106af4ff1c3e9d85f2
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-07 16:21:24 +0600

    Reverse Linked List

--- LeetCode_September_Challenge_2021/reverse_linked_list.cpp
+++ LeetCode_September_Challenge_2021/reverse_linked_list.cpp
@@ -17,3 +17,19 @@ public:
         return pre;
     }
 };
+
+//Recursive Approach
+//Time Complexity O(n)
+//Space Complexity O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        
+        ListNode* newHead=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        
+        return newHead;
+    }
+};
