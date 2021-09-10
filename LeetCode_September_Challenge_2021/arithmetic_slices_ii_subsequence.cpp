2ea958f99d4fd7ac7ff9efab4db070b9fe088520
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-10 23:42:02 +0600

    arithmetic slices ii subsequence

--- LeetCode_September_Challenge_2021/arithmetic_slices_ii_subsequence.cpp
+++ LeetCode_September_Challenge_2021/arithmetic_slices_ii_subsequence.cpp
@@ -21,3 +21,61 @@ public:
         return ans;
     }
 };
+
+
+//Dfs Sample Solution (Invalid)
+class Solution {
+public:
+    unordered_map<pair<int,long long>,int>mp;
+    int dfs(int index,int last,long long diff,vector<int>& A){
+        if(index==A.size()) return 0;
+        
+        pair<int,long long> pr={last,diff};
+        if(mp[pr]) return mp[pr];
+        
+        int local;
+        for(int next=index;next<A.size();next++){
+            if((long long)A[next]-A[last]==diff){
+                local+=dfs(next+1,next,diff,A)+1;
+            }
+        }
+        mp[pr]=local;
+        return local;
+    }
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int n=A.size();
+        if(n < 3) return 0;
+        int total=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                total+=dfs(j+1,j,(long long)A[i]-A[j],A);
+            }
+        }
+        return total;
+    }
+};
+
+//Slow Slution
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int i, j, k, n = A.size(), ans = 0;
+        vector<vector<int>> d(n, vector<int>(n, 0));
+        for (i = 0; i < n - 2; i++) {
+            for (j = i + 1; j < n - 1; j++) {
+                for (k = j + 1; k < n; k++) {
+                    if ((long long)A[i] - A[j] == (long long)A[j] - A[k]) {
+                        d[j][k] = d[j][k] + d[i][j] + 1;
+                    }
+                }
+            }
+        }
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                ans += d[i][j];
+            }
+        }
+        return ans;
+    }
+};
