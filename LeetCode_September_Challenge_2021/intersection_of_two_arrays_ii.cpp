31b5da7adde40ae3b9b29b3e3d7c1b3f7a92b5e2
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-09-17 15:52:19 +0600

    Intersection of two arrays ii

--- LeetCode_September_Challenge_2021/intersection_of_two_arrays_ii.cpp
+++ LeetCode_September_Challenge_2021/intersection_of_two_arrays_ii.cpp
@@ -40,3 +40,51 @@ public:
         return ans;
     }
 };
+
+
+//Time Complexity O(logn)
+//Space Complexity O(1)
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ans;
+        set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(ans));
+        return ans;
+    }
+}
+
+
+//Time Complexity O(n) (Two Pointer Approach)
+//Space Complexity O(1)
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());   //sort the arrays
+        sort(nums2.begin(),nums2.end());
+        vector<int>ans;
+        int i=0;
+        int j=0;
+        int m=nums1.size();
+        int n=nums2.size();
+        while(i<m and j<n){
+            if(nums1[i]==nums2[j])          //if the values are equal , we insert the value into the array 
+			{
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j])      //else we keep on increasing the pointer accordingly 
+			{                                               
+                i++;
+            }
+            else
+			{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
