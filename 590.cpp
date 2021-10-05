a9043565b4114e9051263bfcb6e87970a1d51aea
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-06 00:11:48 +0600

    N-ary tree postorder traversal

--- 590.cpp
+++ 590.cpp
@@ -1,5 +1,5 @@
 
-
+//Recusive (DFS)
 class Solution {
 public:
     void dfs(Node* root,vector<int>&ans){
@@ -16,6 +16,51 @@ public:
         
         dfs(root,ans);
         
+        return ans;
+    }
+};
+
+
+//Iterative 1
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if(!root) return {};
+        vector<int> ans;
+        
+        stack<Node*>st;
+        st.push(root);
+
+        while(!st.empty()){
+            auto x=st.top();
+            st.pop();
+            ans.push_back(x->val);
+            for(auto child:x->children){
+                st.push(child);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//Iterative 2
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorder(Node* root) {
+        if (!root) return {};
+        vector<Node*> stk{root};
+        for (; !stk.empty();) {
+            auto r = stk.back();
+            if (r->children.empty()) {
+                ans.push_back(r->val);
+                stk.pop_back();
+            }
+            for (int i = r->children.size() - 1; i >= 0; i--)
+                stk.push_back(r->children[i]);
+            r->children.clear();
+        }
         return ans;
     }
 };
