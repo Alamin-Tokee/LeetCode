c44314995dddccb86b2994943e93212a8d2e958d
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-11 21:48:30 +0600

    Sum of unique elements

---
+++ 1748.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int sum=0;
+        for(pair<int,int> x:mp){
+            if(x.second==1){
+                sum+=x.first;
+            }
+        }
+        return sum;
+    }
+};
