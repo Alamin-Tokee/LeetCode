d4eeff9504aa2d2df2e13867e9a7d76bd4e0de00
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-14 22:17:49 +0600

    Rotten orange updated

--- 994.cpp
+++ 994.cpp
@@ -1,3 +1,4 @@
+//BFS Solution
 class Solution {
 public:
     int bfs(vector<vector<int>>& g,queue<pair<int,int>>&q){
@@ -52,6 +53,44 @@ public:
     }
 };
 
+
+
+//DFS Solution
+class Solution {
+public:
+    //int offsets[] = {0, 1, 0, -1, 0};
+    void dfs(vector<vector<int>>& g,int r,int c,int l){
+        if(r<0 || c<0 || r>=g.size() || c>=g[0].size() || g[r][c]==0 || (1<g[r][c]&&g[r][c]<l)) return;
+        
+        g[r][c]=l;
+        dfs(g,r+1,c,l+1);
+        dfs(g,r-1,c,l+1);
+        dfs(g,r,c+1,l+1);
+        dfs(g,r,c-1,l+1);
+    }
+    int orangesRotting(vector<vector<int>>& grid) {  
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=2,c=0;
+        if(grid.size() == 0) return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    dfs(grid,i,j,2);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    c++;
+                }
+                ans=max(ans,grid[i][j]);
+            }
+        }
+        return c ? -1 : ans-2; 
+    }
+};
 
 
 
