a7058fe7371c5f65b47f6065ccd68787fbef1e2b
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-21 13:02:45 +0600

    Two Sum

---
+++ 1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>>arr(nums.size());
+        for(int i=0;i<nums.size();i++){
+            arr[i]=make_pair(nums[i],i);
+        }
+        sort(arr.begin(),arr.end());
+        int left = 0, right = nums.size()-1;
+        
+        while(left < right){
+            int sum = arr[left].first + arr[right].first;
+            if(sum == target){
+                return {arr[left].second,arr[right].second};
+            }else if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return {};
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int val = target - nums[i];
+            if(mp.count(val)){
+                return {mp[val],i};
+            }
+            mp[nums[i]] = i;
+            
+        }
+        return {};
+    }
+};
