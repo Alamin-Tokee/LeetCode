611c6ddacc625d5654e8c2423bb572c86ed69055
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-27 16:45:23 +0600

    Climbing stairs

--- 70.cpp
+++ 70.cpp
@@ -52,3 +52,18 @@ public:
     
     }
 };
+
+class Solution {
+public:
+    int dp[46];
+    int solve(int n){    
+        if(n==1 or n==0) return 1;   
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);    
+    }
+    
+    int climbStairs(int n) {
+         memset(dp,-1,sizeof(dp));
+         return solve(n);
+    }
+};
