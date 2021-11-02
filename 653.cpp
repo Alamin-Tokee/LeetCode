80d88e8942bd2a59aba6258126531fa2c8c4d961
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-02 14:53:48 +0600

    Two sum IV - input is a BST

---
+++ 653.cpp
@@ -0,0 +1,84 @@
+
+class Solution {
+public:
+    bool dfs(TreeNode* root,int k,unordered_set<int>&st){
+        if(!root) return false;
+        
+        if(st.count(k-root->val)) return true;
+        st.insert(root->val);
+        
+        return dfs(root->left,k,st) || dfs(root->right,k,st);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int>st;
+        
+        return dfs(root,k,st);
+    }
+};
+
+class Solution {
+public:
+    vector<int>vec;
+    void dfs(TreeNode* root){
+        if(!root) return;
+        dfs(root->left);
+        vec.push_back(root->val);
+        dfs(root->right);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        dfs(root);
+        int l=0, r=vec.size()-1;
+        while(l<r){
+            if(vec[l]+vec[r]==k) return true;
+            else if(vec[l] < k-vec[r]){
+                l++;
+            }else if(vec[l] > k-vec[r]){
+                r--;
+            }
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return true;
+        unordered_map<int,int>mp;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* curr=q.front();
+            q.pop();
+            if(mp.count(k-curr->val) > 0) return true;
+            mp[curr->val]++;
+            if(curr->left) q.push(curr->left);
+            
+            if(curr->right) q.push(curr->right);
+        }
+        
+        return false;
+    }
+};
+
+
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    bool ans=false;
+    void dfs(TreeNode* root,int k){
+        if(!root) return;
+        
+        if(mp.count(k-root->val)) {
+            ans=true;
+        }
+        mp[root->val]++;
+        dfs(root->left,k); 
+        dfs(root->right,k);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        this->ans=ans;
+        dfs(root,k);
+        return ans;
+    }
+};
