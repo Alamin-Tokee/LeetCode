62219947bb814d2d5aaa29bb94906c6a08ea60a7
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-04 15:15:44 +0600

    Find peak element

---
+++ 162.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int binarySearch(vector<int>&nums,int l,int r){
+        if(l==r) return l;
+        int mid=(l+r)/2;
+        if(nums[mid] > nums[mid+1]) return binarySearch(nums,l,mid);
+        
+        return binarySearch(nums,mid+1,r);
+    }
+    int findPeakElement(vector<int>& nums) {
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i] > nums[i+1]) return i;
+//         }
+        
+//         return nums.size()-1;
+        return binarySearch(nums,0,nums.size()-1);
+    }
+};
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid] > nums[mid+1]) 
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+};
