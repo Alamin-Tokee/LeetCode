cc201bed7b5397a421809a9564486dd7511967be
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-05 18:25:44 +0600

    Unique binary search trees

---
+++ 96.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[20];
+    int solve(int n){
+        if(n <= 1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        
+        int ans=0;
+        for(int i=0;i<n;i++){
+             ans += (solve(i) * solve(n - i - 1));
+        }
+        return dp[n]=ans;
+    }
+    int numTrees(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
+
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int>catalan(n+1,0);
+        catalan[0]=catalan[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                catalan[i]+=catalan[j]*catalan[i-j-1];
+            }
+        }
+        return catalan[n];
+    }
+};
