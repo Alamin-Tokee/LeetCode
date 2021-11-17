48f35faecf05cb70b105671bfbeb021eea4a1579
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-17 16:55:11 +0600

    Decode ways

--- 91.cpp
+++ 91.cpp
@@ -70,3 +70,27 @@ public:
         return helper(s,0);
     }
 };
+
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+         int n = s.size();
+    
+        // int count[n+1];
+        vector<int>count(n+1,0);
+        count[0] = 1;
+        count[1] = 1;
+    
+        if(s[0]=='0') return 0;
+    
+        for (int i = 2; i <= n; i++){
+            if (s[i-1] > '0') 
+                count[i] = count[i-1];
+            if (s[i-2] == '1' || (s[i-2] == '2' && s[i-1] < '7') )
+                count[i] += count[i-2];
+        }
+        return count[n];
+    }
+};
