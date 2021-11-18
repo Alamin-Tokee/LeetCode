0b7b84ca42bc079e6d285eb073fb26618a3bce83
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-18 15:34:18 +0600

    Longest incresing subsequence

--- 300.cpp
+++ 300.cpp
@@ -14,3 +14,36 @@ public:
         return ans;
     }
 };
+
+//Time Complexity O(nlogn)
+//Space Complexity O(n)
+class Solution {
+public:
+    int ceilIndex(vector<int>&nums,int l,int h,int x){
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(nums[mid] >= x)
+                h=mid;
+            else
+                l=mid+1;
+        }
+        return h;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>lis(n,0);
+        lis[0]=nums[0];
+        int len=1;
+        
+        for(int i=1;i<n;++i){
+            if(nums[i] > lis[len-1]){
+                lis[len]=nums[i];
+                len++;
+            }else{
+                int c=ceilIndex(lis,0,len-1,nums[i]);
+                lis[c]=nums[i];
+            }
+        }
+        return len;
+    }
+};
