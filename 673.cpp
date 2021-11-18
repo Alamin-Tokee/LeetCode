e16be3384cc8187ba864b6d99739ae389ce665d5
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-18 15:36:00 +0600

    Number of longest increasing subsequence

---
+++ 673.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size(), length = 0,ans = 0;
+        vector<int>dp(n,1);
+        vector<int>count(n,1);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    if(dp[j]+1 > dp[i]){
+                        dp[i]=dp[j]+1;
+                        count[i]=count[j];
+                    }else if(dp[j]+1==dp[i]){
+                        count[i]+=count[j];
+                    }
+                }
+            }
+            length=max(length,dp[i]);
+        }
+        
+        for(int i=0;i<n;i++){
+            if(length == dp[i]) ans+=count[i];
+        }
+        
+        return ans;
+    }
+};
