aeef301a345212ca6ee6e2e39793d40937af7f24
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-11-24 13:24:55 +0600

    Interval list intersection

--- 986.cpp
+++ 986.cpp
@@ -1,3 +1,6 @@
+//Time complexity O(M + N) where m,n are the length of A,B
+//Space Complexity O(M + N) the maximum size of the answer
+
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
@@ -13,6 +16,26 @@ public:
                 ans.push_back({lo,hi});
             }
             A[i][1] < B[j][1] ? i++ : j++;
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int m=A.size();
+        int n=B.size();
+        int i=0,j=0;
+        vector<vector<int>>ans;
+        while(i < m && j < n){
+            pair<int,int>first={A[i][0],A[i][1]}, second={B[j][0],B[j][1]};
+            if(first.second >= second.first && first.first <= second.second){
+                ans.push_back({max(first.first,second.first),min(first.second,second.second)});
+            }
+            if(first.second < second.second) i++;
+            else j++;
         }
         return ans;
     }
