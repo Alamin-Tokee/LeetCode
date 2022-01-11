065d65de5754ad1a72d1dcad74fd64bb1a3fcdca
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2022-01-11 23:42:59 +0600

    Sum of root to leaf binary numbers

---
+++ LeetCode_January_Challenge_2022/sum_of_root_to_leaf_binary_numbers.cpp
@@ -0,0 +1,47 @@
+//Time Complexity O(N)
+//Space Complexity O(H)
+
+class Solution {
+public:
+    int total=0;
+    void dfs(TreeNode* root,int sum){
+        if(!root) return;
+        sum=(sum <<1) | root->val;
+        if(root->left==NULL and root->right==NULL) total+=sum;
+        dfs(root->left,sum);
+        dfs(root->right,sum);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        if(!root) return 0;
+        this->total=total;
+        dfs(root,0);
+        
+        return total;
+    }
+};
+
+//Time Complexity O(N)
+//Space Complexity O(H)
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        if(!root) return 0;
+        int total=0;
+        int curSum=0;
+        stack<pair<TreeNode*,int>>st;
+        st.push({root,0});
+        while(!st.empty()){
+            TreeNode* x=st.top().first;
+            curSum=st.top().second;
+            st.pop();
+            curSum=(curSum<<1)|x->val;
+            if(x->left==NULL and x->right==NULL) total+=curSum;
+            
+            if(x->left) st.push(make_pair(x->left,curSum));
+            if(x->right) st.push(make_pair(x->right,curSum));
+            
+        }
+        
+        return total;
+    }
+};
